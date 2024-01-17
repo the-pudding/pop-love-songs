264b54ef5d945736820fc3f8598efcd9973f4197
David Mora <davidnmora@gmail.com>
2024-01-17 13:07:52 -0800

    Try and fail to get lovesongtypefilter working with 2-way sync (grrrr)

--- src/components/viz/search-and-filter-top-bar/LoveSongTypeFilter.svelte
+++ src/components/viz/search-and-filter-top-bar/LoveSongTypeFilter.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onDestroy } from "svelte";
 	import { LOVE_SONG_TYPES } from "$data/data-constants";
 	import searchAndFilter from "$stores/searchAndFilter.js";
 	import MultiSelect from "svelte-multiselect"; // (eventually we'll replace this with our own Select component likely)
@@ -10,12 +11,23 @@
 
 	let userSelectedLoveSongTypes = [];
 
+	// Subscribe to the searchAndFilter store, update the local variable when it changes
+	const unsubscribe = searchAndFilter.subscribe(($searchAndFilter) => {
+		// if ($searchAndFilter.selectedLoveSongTypes.length === userSelectedLoveSongTypes.length) return
+		// setting userSelectedLoveSongTypes instead of "t" causes an infinite loop... @michelle how do I fix this?
+		const t = loveSongOptions.filter(option =>
+			$searchAndFilter.selectedLoveSongTypes.includes(option.value)
+		);
+		console.log(t)
+	});
+
 	$: {
 		searchAndFilter.update((state) => ({
 			...state,
 			selectedLoveSongTypes: userSelectedLoveSongTypes.map(({ value }) => value)
 		}));
 	}
+	onDestroy(unsubscribe);
 </script>
 
 <MultiSelect
