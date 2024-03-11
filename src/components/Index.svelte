190a49775ad4540e6e83c6e071c2784f98f31147
David Mora <davidnmora@gmail.com>
2024-03-10 23:07:18 -0700

    Rename for clarity

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,6 +1,6 @@
 <script>
 	import MainViz from "./viz/MainViz.svelte";
-	import ListViewSideBar from "./viz/ListViewSideBar.svelte";
+	import SideBar from "./viz/SideBar.svelte";
 	import StoryStepController from "./story-steps/StoryStepController.svelte";
 	import SongTooltip from "./viz/SongTooltip.svelte";
 	import SearchAndFilterTopBar from "./viz/search-and-filter-top-bar/SearchAndFilterTopBar.svelte";
@@ -8,6 +8,6 @@
 
 <SearchAndFilterTopBar />
 <MainViz />
-<ListViewSideBar />
+<SideBar />
 <StoryStepController />
 <SongTooltip />
