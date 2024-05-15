39f730c443512ecf8211b0a3a98098998c08ee34
David Mora <davidnmora@gmail.com>
2024-05-15 08:56:04 -0700

    Refactor label metadata into a derive store

--- src/components/viz/LoveSongTypeLabels.svelte
+++ src/components/viz/LoveSongTypeLabels.svelte
@@ -1,29 +1,35 @@
 <script>
+	import { derived } from "svelte/store";
+	
 	import { LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP } from "$data/data-constants";
 	import viewport from "$stores/viewport";
 	import { visibleButNotSelectedLoveSongTypes } from "$stores/searchAndFilter";
 	import { getXPosForYear } from "$data/data-utils";
 	import { getYPosForPercentage } from "$stores/forcePositionOptions-helper";
+	
 
     export let tweenedCoords;
 
-    // TODO: maybe make this a pre-derived store?
-    $: getLabelCoords = (svgCoords) => {
-		const {x, y0, y1} = svgCoords[0]
-		return {
-			x: getXPosForYear(x, $viewport.width),
-			// TODO: @michelle there's gotta be a better way to align text to the baseline via css...
-			y: getYPosForPercentage(y0, $viewport.height) - 0.035 * $viewport.height
+	// @michelle: is this an anti-pattern? ie mixing store derivation with reactive statements
+	$: labelMetadata = derived(
+		[visibleButNotSelectedLoveSongTypes, viewport],
+		([$visibleButNotSelectedLoveSongTypes, $viewport]) => {
+			return tweenedCoords.map(({ loveSongType, svgCoords }) => ({
+				loveSongType,
+				x: getXPosForYear(svgCoords[0].x, $viewport.width),
+				// TODO: @michelle there's gotta be a better way to align text to the baseline via css...
+				// TODO: Also, I think I need to factor in the conditional margin between snakes...
+				y: getYPosForPercentage(svgCoords[0].y0, $viewport.height) - 0.035 * $viewport.height,
+				opacity: $visibleButNotSelectedLoveSongTypes.includes(loveSongType) ? 0 : 1
+			}))
 		}
-	};
+	);
 
 </script>
-{#each tweenedCoords as { loveSongType, svgCoords }}
-		<div class="snake-label" style:left={`${getLabelCoords(svgCoords).x}px`} style:top={`${getLabelCoords(svgCoords).y}px`} fill="black" opacity={
-			$visibleButNotSelectedLoveSongTypes.includes(loveSongType)
-				? 0 
-				: 1
-		}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[loveSongType]}</div>
+{#each $labelMetadata as { loveSongType, x, y, opacity }}
+	<div class="snake-label" style:left={`${x}px`} style:top={`${y}px`} fill="black" opacity={opacity}>
+		{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[loveSongType]}
+	</div>
 {/each}
 
 <style>
