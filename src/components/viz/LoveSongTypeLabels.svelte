b0eb44ae1dfe80060affa4550e9e5037cdb708ef
David Mora <davidnmora@gmail.com>
2024-05-15 09:36:53 -0700

    Try (in vain) to dynamically set font size

--- src/components/viz/LoveSongTypeLabels.svelte
+++ src/components/viz/LoveSongTypeLabels.svelte
@@ -16,7 +16,7 @@
 		([$visibleButNotSelectedLoveSongTypes, $viewport]) => {
 			return tweenedCoords.reduce((acc, { loveSongType, svgCoords }) => {
 				const MIN_Y_HEIGHT_TO_FIT_LABEL = 0.04;
-				const {x, y0} = svgCoords.find(({y0, y1}) => (y0 - y1) >= MIN_Y_HEIGHT_TO_FIT_LABEL) || {};
+				const {x, y0, y1} = svgCoords.find(({y0, y1}) => (y0 - y1) >= MIN_Y_HEIGHT_TO_FIT_LABEL) || {};
 				if (x === undefined || y0 === undefined) return acc;
 
 				return [... acc, {
@@ -25,21 +25,24 @@
 					// TODO: @michelle there's gotta be a better way to align text to the baseline via css...
 					// TODO: Also, I think I need to factor in the conditional margin between snakes...
 					y: getYPosForPercentage(y0, $viewport.height) - 0.035 * $viewport.height,
-					opacity: $visibleButNotSelectedLoveSongTypes.includes(loveSongType) ? 0 : 1
+					opacity: $visibleButNotSelectedLoveSongTypes.includes(loveSongType) ? 0 : 1,
+					// TODO: for some reason, I can't seem to set the style:fontSize property on the dev... ?
+					fontSize: (y0 - y1) < 2 * MIN_Y_HEIGHT_TO_FIT_LABEL ? "clamp(0.5rem, 2vw, .75rem)" : "clamp(1rem, 2vw, 1.25rem)"
 				}]
 			}, []);
 		}
 	);
 
 </script>
-{#each $labelMetadata as { loveSongType, x, y, opacity }}
-	<div class="snake-label" style:left={`${x}px`} style:top={`${y}px`} fill="black" opacity={opacity}>
+{#each $labelMetadata as { loveSongType, x, y, opacity, fontSize }}
+	<div class="snake-label" style:left={`${x}px`} style:top={`${y}px`} fill="black" style:opacity={opacity} style:fontSize={fontSize}>
 		{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[loveSongType]}
 	</div>
 {/each}
 
 <style>
     div.snake-label {
+		/* TODO: For some reason I can only set font-size via css... */
 		font-size: clamp(1rem, 2vw, 1.25rem);
 		position: fixed;
 	}
