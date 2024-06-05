96dc99b9d3d59c49ca590b83581ba6461f12d3f7
David Mora <davidnmora@gmail.com>
2024-06-05 12:10:18 -0700

    Add rudimentary modern percentage change

--- src/components/viz/LoveSongChangeAnnotation.svelte
+++ src/components/viz/LoveSongChangeAnnotation.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { LOVE_SONG_TYPE_CONSTANTS } from "$data/data-constants";
-	import {  MIN_DATE, MAX_DATE } from "$data/songs-data";
+	import {  MIN_DATE } from "$data/songs-data";
     import { onlyShowOneDecimalPlaceIfLessThan10 } from "$data/data-utils";
     
     import viewport from "$stores/viewport";
@@ -9,17 +9,19 @@
 	
     export let tweenedCoords;
 
-    $: sixtiesYScreenPercentage = tweenedCoords && tweenedCoords.find(({loveSongType}) => +loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.find(({x}) => x === MIN_DATE).y0
+    $: sixtiesYScreenPercentage = tweenedCoords.find(({loveSongType}) => +loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.find(({x}) => x === MIN_DATE).y0
     $: sixtiesYPos = getYPosForPercentage(sixtiesYScreenPercentage, $viewport.height)
-    // $: modernYScreenPercentage = tweenedCoords && tweenedCoords.find(({loveSongType}) => loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.find(({x}) => x === MAX_DATE).y0
+    
+    // TODO: (bug?) the max value of the tweenedCoords does not === MAX_DATE... why? Is this a mistake in how we're laying out the coords?
+    $: modernYScreenPercentage = tweenedCoords.find(({loveSongType}) => loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.at(-1).y0
+    $: modernYPos = getYPosForPercentage(modernYScreenPercentage, $viewport.height)
 </script>
 
 {#if $currentStoryStep.showLoveSongChange}
     <div>
-        <div style:top={`${sixtiesYPos}px`}>60s: {onlyShowOneDecimalPlaceIfLessThan10(sixtiesYScreenPercentage)}</div>
-        <!-- <div style:top={`${sixtiesYPos}px`}>now: {onlyShowOneDecimalPlaceIfLessThan10(sixtiesYScreenPercentage)}</div> -->
-        <!-- <div>[% change]</div>
-        <div>now</div> -->
+        <div style:top={`${sixtiesYPos}px`}>60s: {onlyShowOneDecimalPlaceIfLessThan10(1 - sixtiesYScreenPercentage)}</div>
+        <div style:top={`${modernYPos}px`}>now: {onlyShowOneDecimalPlaceIfLessThan10(1 - modernYScreenPercentage)}</div>
+        <!-- <div>[% change]</div> -->
     </div>
 {/if}
 
