c97f96b8418ae3f614df76f7f657381b96708d37
David Mora <davidnmora@gmail.com>
2024-06-10 15:24:07 -0700

    Hide expansive column when its empty

--- src/components/love-song-definition-table/LoveSongDefinitionTable.svelte
+++ src/components/love-song-definition-table/LoveSongDefinitionTable.svelte
@@ -8,6 +8,12 @@
         return loveSongTypesInRow.some(loveSongType => !$currentStoryStep.searchAndFilterState.typesTreatedAsNonLoveSongs.some(t => t === loveSongType));
     }
 
+    const SECOND_COLUMN_TYPES = [LOVE_SONG_TYPE_CONSTANTS.sexualConquest, LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf, LOVE_SONG_TYPE_CONSTANTS.itsComplicated, LOVE_SONG_TYPE_CONSTANTS.goodRiddance];
+    $: secondColumnIsVisible = () => {
+        // If any of the types in the second column are not treated as non-love songs, then the second column should NOT be visible
+        return SECOND_COLUMN_TYPES.some(loveSongType => !$currentStoryStep.searchAndFilterState.typesTreatedAsNonLoveSongs.some(t => t === loveSongType));
+    }
+
     $: typeOpacity = (loveSongType) => {
         const isTheHighlightedType = $currentStoryStep.showLoveSongTypeTableWithThisHighlighted === loveSongType;
         if (isTheHighlightedType) return 1;
@@ -30,7 +36,10 @@
     <tr>
         <td></td>
         <th scope="col">“You love(d) them, & they...</th>
-        <th scope="col"></th>
+        {#if secondColumnIsVisible()}
+            <th scope="col"></th>
+        {/if}
+        
     </tr>
 
     {#if rowIsVisible([LOVE_SONG_TYPE_CONSTANTS.courtshipAndAnticipation, LOVE_SONG_TYPE_CONSTANTS.sexualConquest])}
@@ -43,20 +52,24 @@
         <tr>
             <th scope="row">... love you back ❤️”</th>
             <td style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.serenade)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.serenade]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.serenade]}</td>
-            <td>
-                <div style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.sexualConquest)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.sexualConquest]} class="straddles-cell-above">{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.sexualConquest]}</div>
-                <div style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf]}</div>
-            </td>
+            {#if secondColumnIsVisible()}
+                <td>
+                    <div style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.sexualConquest)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.sexualConquest]} class="straddles-cell-above">{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.sexualConquest]}</div>
+                    <div style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.loveSongForTheSelf]}</div>
+                </td>
+            {/if}
         </tr>
     {/if}
     {#if rowIsVisible([LOVE_SONG_TYPE_CONSTANTS.longingAndHeartbreak, LOVE_SONG_TYPE_CONSTANTS.goodRiddance])}
         <tr>
             <th scope="row">... don't love you (but might have once)”</th>
             <td style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.longingAndHeartbreak)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.longingAndHeartbreak]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.longingAndHeartbreak]}</td>
-            <td>
-                <div class="straddles-cell-above" style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.itsComplicated)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.itsComplicated]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.itsComplicated]}</div>
-                <div style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.goodRiddance)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.goodRiddance]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.goodRiddance]}</div>
-            </td>
+            {#if secondColumnIsVisible()} 
+                <td>
+                    <div class="straddles-cell-above" style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.itsComplicated)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.itsComplicated]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.itsComplicated]}</div>
+                    <div style:opacity={typeOpacity(LOVE_SONG_TYPE_CONSTANTS.goodRiddance)} style:color={LOVE_SONG_TYPE_COLOR_MAP[LOVE_SONG_TYPE_CONSTANTS.goodRiddance]}>{LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[LOVE_SONG_TYPE_CONSTANTS.goodRiddance]}</div>
+                </td>
+            {/if}
         </tr>
     {/if}
 </table>
