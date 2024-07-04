1c5dfc27a287bda16b3c12ea31dedb9d2c80df3d
David Mora <davidnmora@gmail.com>
2024-07-03 18:48:45 -0700

    When not in x-axis mode (ie random bubbles), place annotations exactly next to bubble (target)

--- src/components/viz/SongAnnotations.svelte
+++ src/components/viz/SongAnnotations.svelte
@@ -4,9 +4,11 @@
 
     import viewport from "$stores/viewport";
 
+    import { currentStoryStep } from "$stores/storySteps";
     import { songAnnotationsWithPosition } from "$stores/visualEncodings";
 	import SongInfo from "./SongInfo.svelte";
 	import { CHART_TRANSITION_OPACITY_DURATION } from "./viz-utils";
+	
 
     $: y = $viewport.height / 2;
 
@@ -15,16 +17,20 @@
         const xOffset =  ANNOTATION_WIDTH > $viewport.width - x ? - ANNOTATION_WIDTH : 10;
         return x + xOffset
     }
-    
+
+    $: getYPos = (y, isLastAnnotation, overlapsWithNext) => {
+        if ($currentStoryStep.showXAxis) return !isLastAnnotation && overlapsWithNext ? y - 120 : y + 40;
+        return y
+    }
+
     // create a reactive value that calculates the x position for each annotaiton, then if any overlap, it gives sets the y position with a negative offset
     $: layoutData = $songAnnotationsWithPosition.map(({song, x, y}, i) => {
         const xPos = getXPos(x);
-        // check if we're at the last element
-        if (i === $songAnnotationsWithPosition.length - 1) {
-            return {xPos, yPos: y, song}
-        }
-        const nextXPos = getXPos($songAnnotationsWithPosition[i + 1].x);
-        return {xPos, yPos: nextXPos - xPos < ANNOTATION_WIDTH ? y - 180 : y, song}
+        const isLastAnnotation = i === $songAnnotationsWithPosition.length - 1;
+        const nextXPos = isLastAnnotation ? null : getXPos($songAnnotationsWithPosition[i + 1].x);
+        const overlapsWithNext = nextXPos - xPos < ANNOTATION_WIDTH;
+        const yPos = getYPos(y, isLastAnnotation, overlapsWithNext)
+        return {xPos, yPos, song}
     })
 
     // @michelle: is it better to just use CSS transition?
