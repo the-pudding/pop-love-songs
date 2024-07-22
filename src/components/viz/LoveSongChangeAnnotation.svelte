f8d347e483f88b4401cf7d5631024d0e0bc74092
David Mora <davidnmora@gmail.com>
2024-07-21 20:38:01 -0700

    WIP: get placeholder for now love song change annotation

--- src/components/viz/LoveSongChangeAnnotation.svelte
+++ src/components/viz/LoveSongChangeAnnotation.svelte
@@ -5,7 +5,7 @@
     import viewport from "$stores/viewport";
     import { currentStoryStep } from "$stores/storySteps";
 	import { getYPosForPercentage } from "$stores/forcePositionOptions-helper";
-	import { formattedLoveSongPercentChange } from "$stores/dataDerivations";
+	import TotalLoveSongPercentageAnnotation from "./TotalLoveSongPercentageAnnotation.svelte";
 	
     export let tweenedCoords;
 
@@ -15,54 +15,16 @@
     // TODO: (bug?) the max value of the tweenedCoords does not === MAX_DATE... why? Is this a mistake in how we're laying out the coords?
     $: modernYScreenPercentage = tweenedCoords.find(({loveSongType}) => loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.at(-1).y0
     $: modernYPos = getYPosForPercentage(modernYScreenPercentage, $viewport.height)
-
-    $: changeTextPos = (sixtiesYPos + modernYPos) / 2
 </script>
 
 {#if $currentStoryStep.showLoveSongChange}
     <div>
-        <div style:top={`${sixtiesYPos}px`} class="guideline"></div>
-        <div style:top={`${sixtiesYPos}px`} class="baseline-text">60s</div>
 
-        <div style:top={`${modernYPos}px`} class="guideline"></div>
-        <div style:top={`${modernYPos}px`}>now</div>
-        
-        <div style:top={`${changeTextPos}px`} class="change">{$formattedLoveSongPercentChange}% change</div>
-    
+       <TotalLoveSongPercentageAnnotation topPosition={sixtiesYPos} year="1960s" percentage={"Y"} isLeftSide={true} />
+       <TotalLoveSongPercentageAnnotation topPosition={modernYPos} year="2020s" percentage={"X"} />
     </div>
 {/if}
 
 <style>
-    div div {
-        z-index: 100000;
-        position: fixed;
-        right: 20px;
-        pointer-events: none;
-
-        font-family: 'Atlas Grotesk', sans-serif;
-    }
-
-    div div.baseline-text {
-        top: 50%;
-        transform: translateY(-100%);
-        display: flex;
-        align-items: baseline;
-    }
-
-    div div.guideline {
-        width: 100%;
-        height: 1px;
-        border-top: 1px dashed black;
-    }
-
-    div .change {
-        margin-right: 24px;
-
-        top: 50%;
-        transform: translateY(-50%);
-        display: flex;
-        align-items: baseline;
-        font-weight: bold;
-        font-size: 24px;
-    }
+    
 </style>
---
+++ src/components/viz/TotalLoveSongPercentageAnnotation.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let topPosition = 0;
+    export let year = "";
+    export let percentage = 0;
+    export let isLeftSide = false;
+
+    // TODO: decide left/right margins more intelligently
+    $: style = `top: ${topPosition}px; ${isLeftSide ? "left: 50px;" : "right: 50px;"}`;
+</script>
+
+<div class="container" style={style}>
+    <div class="year">2020s</div>
+    <div class="percentage">Y%</div>
+    <div class="love-songs">love songs</div>
+</div>   
+
+<style>
+    .container {
+        z-index: 100000;
+        position: fixed;
+        right: 20px;
+        /* pointer-events: none; */
+
+        font-family: 'Atlas Grotesk', sans-serif;
+    }
+    .year, .love-songs {
+        font-size: 12px;
+    }
+
+    .year, .percentage {
+        font-weight: bold;
+    }
+
+    .percentage {
+        font-size: 24px;
+    }
+</style>
