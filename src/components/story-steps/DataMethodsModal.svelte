9471484d8bd0b15f9aa3d5b992310add67725898
David Mora <davidnmora@gmail.com>
2024-08-21 18:21:10 -0700

    Add modal text from gdoc

--- src/components/story-steps/DataMethodsModal.svelte
+++ src/components/story-steps/DataMethodsModal.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Modal from '../helpers/Modal.svelte';
+    import copy from "$data/copy.json";
     
+    const { dataMethods } = copy;
+
     export let buttonText = 'show modal';
 
 	let showModal = false;
@@ -10,4 +13,12 @@
 
 <Modal bind:showModal>
 	<h1>Data Methods</h1>
+    
+    <!-- Iterate over dataMethods -->
+    {#each dataMethods as {title, paragraphs}}
+        <h2>{title}</h2>
+        {#each paragraphs as paragraph}
+            <p>{@html paragraph}</p>
+        {/each}
+    {/each}
 </Modal>
