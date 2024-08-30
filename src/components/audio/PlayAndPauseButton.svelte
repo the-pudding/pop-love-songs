a650626ad7551e0467599373e1a65e525f2604a2
David Mora <davidnmora@gmail.com>
2024-08-30 16:35:30 -0700

    Scale down play button

--- src/components/audio/PlayAndPauseButton.svelte
+++ src/components/audio/PlayAndPauseButton.svelte
@@ -3,7 +3,7 @@
     export let paused = true;
 </script>
 
-<button class:paused class='button'></button>
+<div class:paused class='button'></div>
 
 <style>
     .button {
@@ -12,16 +12,19 @@
         background: transparent;
         box-sizing: border-box;
         width: 0;
-        height: 74px;
+        /* height: 74px; */
+        height: 24px;
         border-color: transparent transparent transparent #202020;
         transition: 100ms all ease;
         cursor: pointer;
         border-style: solid;
-        border-width: 37px 0 37px 60px;
+        /* border-width: 37px 0 37px 60px; */
+        border-width: 12px 0 12px 19.4px;
     }
     .button.paused {
         border-style: double;
-        border-width: 0px 0 0px 60px;
+        /* border-width: 0px 0 0px 60px; */
+        border-width: 0px 0 0px 24px;
     }
     .button:hover {
         border-color: transparent transparent transparent #404040;
