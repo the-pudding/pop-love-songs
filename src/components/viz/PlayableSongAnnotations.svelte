8451075eb751ad75337cdebc06c983b998e7f15b
David Mora <davidnmora@gmail.com>
2024-09-04 20:04:32 -0700

    WIP Draw a line between bubble and playable annotation

--- src/components/viz/PlayableSongAnnotations.svelte
+++ src/components/viz/PlayableSongAnnotations.svelte
@@ -25,30 +25,47 @@
         .map(({x, y, song, rightAlign, placeBelow, placeAbove, alternateTitle, audioFile}) => {
             const xPos = getX(x, rightAlign, placeBelow, placeAbove);
             const yPos = -200 + y - (placeBelow ? -40 : (placeAbove ? 60 : Y_OFFSET)); // TODO: make this not stupid
-            return {xPos, yPos, song, rightAlign, alternateTitle, audioFile}
+            return {bubbleX: x, bubbleY: y, xPos, yPos, song, rightAlign, alternateTitle, audioFile}
         })
 </script>
 
-{#each layoutData as {xPos, yPos, song, rightAlign, alternateTitle, audioFile}}
+{#each layoutData as {bubbleX, bubbleY, xPos, yPos, song, rightAlign, alternateTitle, audioFile}}
     <div
         id={song[SONG_DATA_COLUMNS_ENUM.song]}
         class="annotation-wrapper"
         role="tooltip"
         in:fade={{delay: CHART_TRANSITION_OPACITY_DURATION / 2, duration: CHART_TRANSITION_OPACITY_DURATION / 2 }}
-        style={`top: ${yPos}px; left: ${xPos}px; ${rightAlign ? 'transform: translateX(-100%);' : ''}`}
+        style={`top: ${yPos}px; left: ${xPos}px; ${rightAlign ? 'transform: translateX(-100%), translateY(-50%);' : ''}`}
     >
         <SongInfo song={song} alternateTitle={alternateTitle} audioFile={audioFile} />
     </div>
 {/each}
 
+<!-- For each layoutData, use html to draw a line extending from bubbleX/Y up to x/yPos with thickness 3 pixels -->
+
+{#each layoutData as {bubbleX, bubbleY, xPos, yPos, song, rightAlign, alternateTitle, audioFile}}
+    <div
+        class="annotation-line"
+        in:fade={{delay: CHART_TRANSITION_OPACITY_DURATION / 2, duration: CHART_TRANSITION_OPACITY_DURATION / 2 }}
+        style={`top: ${yPos}px; left: ${bubbleX}px; height: ${bubbleY - yPos}px;`}
+    />
+{/each}
+
 
 
 <style>
+    div {
+        position: absolute;
+        z-index: 10000;
+    }
+
 	div.annotation-wrapper {
-		z-index: 10000;
-		position: absolute;
 		max-height: 300px;
-		background-color: transparent;
-        transition: opacity 4s;
+    }
+
+    div.annotation-line {
+        background-color: black;
+        width: 2px;
+        margin-left: -0.5px;
     }
 </style>
