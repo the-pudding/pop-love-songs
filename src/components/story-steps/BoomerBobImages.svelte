fca16adcabc90e3ede324db44cd4c12277cc59bf
David Mora <davidnmora@gmail.com>
2024-09-13 19:12:50 -0700

    Fix aspect ratio

--- src/components/story-steps/BoomerBobImages.svelte
+++ src/components/story-steps/BoomerBobImages.svelte
@@ -3,34 +3,36 @@
 	import viewport from "$stores/viewport";
 	import { STORY_STEP_CONTROLLER_TOP_PADDING } from "../viz/viz-utils";
 
-    const BOOMER_BOB_IMAGES = {
-        headlines: {
-            src: "assets/images/love song decline headlines.png",
-            alt: "Screenshots of article headlines reading: 'A generation without love songs', 'RIP love songs', or 'they no longer write love songs like they used to'."
-        }
+    const headlines = {
+        src: "assets/images/love song decline headlines.png",
+        alt: "Screenshots of article headlines relating to the decline of love songs, such as: 'A generation without love songs', 'RIP love songs', 'love songs are in decline', 'love songs are harder to find' and 'they no longer write love songs like they used to'."
     }
 
     $: topMargin = STORY_STEP_CONTROLLER_TOP_PADDING + ($viewport.height * Y_MARGIN_SCREEN_PERCENTAGE)
     $: console.log({topMargin})
     $: style = `
+        top: ${topMargin}px;
+        width: 100%;
         height: ${$viewport.height - topMargin}px;
     `
 </script>
 
-<div class="container">
-    <img {style} src={BOOMER_BOB_IMAGES.headlines.src} alt={BOOMER_BOB_IMAGES.headlines.alt} />
+<div class="container" {style}>
+    <img {style} src={headlines.src} alt={headlines.alt} />
 </div>
 
 <style>
     div.container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        padding: 24px;
     }
 
     img {
-        position: fixed; 
-        left: 50%;
-        transform: translate(-50%, -50%);
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
     }
 </style>
