f9c7f43cb68799dac20091205d32ee5be0765a3a
David Mora <davidnmora@gmail.com>
2024-09-15 19:49:02 -0700

    WIP get tiny love song percentage labels (x is not set yet)

--- src/components/viz/LoveSongChangeAnnotation.svelte
+++ src/components/viz/LoveSongChangeAnnotation.svelte
@@ -9,21 +9,47 @@
     import { showAggregateSnakeChart } from "$stores/searchAndFilter";
 
 	import TotalLoveSongPercentageAnnotation from "./TotalLoveSongPercentageAnnotation.svelte";
+	import { svgCoordsForSnakeChartOutline } from "$stores/snakeChartOutlineGenerator";
+	import LoveSongChangeTinyAnnotation from "./LoveSongChangeTinyAnnotation.svelte";
 	
     export let tweenedCoords;
 
     $: sixtiesYScreenPercentage = tweenedCoords.find(({loveSongType}) => +loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.find(({x}) => x === MIN_DATE).y0
     $: sixtiesYPos = getYPosForPercentage(sixtiesYScreenPercentage, $viewport.height)
     
-    // TODO: (bug?) the max value of the tweenedCoords does not === MAX_DATE... why? Is this a mistake in how we're laying out the coords?
     $: modernYScreenPercentage = tweenedCoords.find(({loveSongType}) => loveSongType === LOVE_SONG_TYPE_CONSTANTS.notALoveSong).svgCoords.at(-1).y0
     $: modernYPos = getYPosForPercentage(modernYScreenPercentage, $viewport.height)
+
+    // From svgCoordsForSnakeChartOutline, for each y1's
+    $: console.log({$svgCoordsForSnakeChartOutline})
+    const SMALL_LABEL_DECADES = [1970, 1980, 1990, 2000, 2010]
+    $: smallLabelYPos = SMALL_LABEL_DECADES.map((decade, index) => {
+        // reduce to get the average of the y1's for all the x's in that decade
+        const screenPercentage = $svgCoordsForSnakeChartOutline.reduce(
+            (acc, {x, y1}) => {
+                if (decade <= x && x < decade + 10) {
+                    acc.y += y1,
+                    acc.x += x
+                }
+                return acc
+            }, 
+            {y: 0, x: 0}
+        )
+        return {
+            y: getYPosForPercentage(screenPercentage.y / 2, $viewport.height),
+            x: 400,
+            decade
+        }
+    })
+    $: console.log({smallLabelYPos})
 </script>
 
 {#if $currentStoryStep.showLoveSongChange && $showAggregateSnakeChart}
     <div>
-
        <TotalLoveSongPercentageAnnotation topPosition={sixtiesYPos} year="1960s" percentage={onlyShowOneDecimalPlaceIfLessThan10(100 * (1 - sixtiesYScreenPercentage))} isLeftSide={true} />
+        {#each smallLabelYPos as {x, y, decade}, i}
+            <LoveSongChangeTinyAnnotation {y} {x} year={`${decade}s`} percentage={onlyShowOneDecimalPlaceIfLessThan10(100 * (1 - $svgCoordsForSnakeChartOutline.find(({x}) => x === decade).y1))}/>
+        {/each}
        <TotalLoveSongPercentageAnnotation topPosition={modernYPos} year="2020s" percentage={onlyShowOneDecimalPlaceIfLessThan10(100 * (1 - modernYScreenPercentage))} />
     </div>
 {/if}
---
+++ src/components/viz/LoveSongChangeTinyAnnotation.svelte
@@ -0,0 +1,32 @@
+<script>
+	import viewport from "$stores/viewport";
+
+    export let x = 0;
+    export let y = 0;
+    export let year = "";
+    export let percentage = 0;
+
+    // TODO: decide left/right margins more intelligently
+    $: style = `top: ${y}px; left: ${x}px;`;
+</script>
+
+{#if !$viewport.isLikelyInMobileLandscape}
+    <div class="container" style={style} aria-label={`In ${year}, love songs made up ${percentage}% of all songs.`}>
+        <div class="percentage">{percentage}%</div>
+    </div>   
+{/if}
+
+<style>
+    .container {
+        position: fixed;
+        transform: translateY(-100%);
+
+        z-index: 100000;
+        font-family: 'Atlas Grotesk', sans-serif;
+    }
+
+    .percentage {
+        font-weight: bold;
+        font-size: 16px;
+    }
+</style>
