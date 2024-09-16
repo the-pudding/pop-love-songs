81f692baa556dcf2957f981344a77546b5682feb
David Mora <davidnmora@gmail.com>
2024-09-15 17:31:26 -0700

    Generate coords for snake chart outline

--- src/stores/snakeChartOutlineGenerator.js
+++ src/stores/snakeChartOutlineGenerator.js
@@ -1,65 +1,30 @@
 import { derived } from "svelte/store";
-import { area as d3area } from "d3";
 import viewport from "./viewport";
 
-import { getYPosInAggregateSnakeChart } from "./forcePositionOptions-helper";
-import { getXPosForYear } from "$data/data-utils";
-import { LOVE_SONG_TYPES } from "$data/data-constants.js";
+import { svgCoordsForLoveSongTypes } from "./aggregateSnakeChartPositions";
 
 const BASE_OF_CHART = 0;
-const createSVGPathGenerator = ($viewport) => {
-	return d3area()
-		.x(({ x }) => getXPosForYear(x, $viewport.width))
-		.y0(({ y0, y1 }) =>
-			getYPosInAggregateSnakeChart({
-				percentage: BASE_OF_CHART,
-				percentageChange: BASE_OF_CHART - y1,
-				canvasHeight: $viewport.height,
-				isY0: true
-			})
-		)
-		.y1(({ y0, y1 }) =>
-			getYPosInAggregateSnakeChart({
-				percentage: y1,
-				canvasHeight: $viewport.height,
-				percentageChange: BASE_OF_CHART - y1
-			})
+
+export const svgCoordsForSnakeChartOutline = derived(
+	[svgCoordsForLoveSongTypes],
+	([$svgCoordsForLoveSongTypes]) => {
+		const timeRegionIndexes = $svgCoordsForLoveSongTypes[0].svgCoords.map(
+			(_, index) => index
 		);
-};
+		const y1MaxForAllLoveSongTypesAtEachX = timeRegionIndexes.map((index) => {
+			const y1Max = $svgCoordsForLoveSongTypes.reduce(
+				(accum, { svgCoords }) => {
+					const { y1, x } = svgCoords[index];
+					return y1 > accum.y1 ? { y0: BASE_OF_CHART, y1, x } : accum;
+				},
+				{ y0: BASE_OF_CHART, y1: -1, x: -1 }
+			);
+			return y1Max;
+		});
 
-const TRANSITION_WIDTH_BETWEEN_TIME_REGIONS = 1.8; // in years. NOTE: regions are already a year apart, accounted for below
-const getSvgCoordsForLoveSongType = (
-	loveSongType,
-	aggregateSnakeChartPositions
-) =>
-	aggregateSnakeChartPositions.reduce((accum, timeRegion) => {
-		const { y0, y1 } =
-			timeRegion.popularityScoreSumsInTimeRegion[loveSongType] || {};
-		return [
-			...accum,
-			{
-				x: timeRegion.start,
-				y0,
-				y1
-			},
-			{
-				x: timeRegion.stop - (TRANSITION_WIDTH_BETWEEN_TIME_REGIONS - 1),
-				y0,
-				y1
-			}
-		];
-	}, []);
-
-export const svgCoordsForLoveSongTypes = derived(
-	[aggregateSnakeChartPositions],
-	([$aggregateSnakeChartPositions]) =>
-		LOVE_SONG_TYPES.map((loveSongType) => ({
-			loveSongType,
-			svgCoords: getSvgCoordsForLoveSongType(
-				loveSongType,
-				$aggregateSnakeChartPositions
-			)
-		}))
+		console.log(y1MaxForAllLoveSongTypesAtEachX);
+		return y1MaxForAllLoveSongTypesAtEachX;
+	}
 );
 
 export const snakeChartOutlinePathGenerator = derived(
