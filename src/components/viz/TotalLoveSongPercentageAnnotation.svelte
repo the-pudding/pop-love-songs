f3c0f5acb514904e002b69548322c7274fa12fff
David Mora <davidnmora@gmail.com>
2024-09-15 20:01:03 -0700

    Clean up html

--- src/components/viz/TotalLoveSongPercentageAnnotation.svelte
+++ src/components/viz/TotalLoveSongPercentageAnnotation.svelte
@@ -7,11 +7,11 @@
     export let isLeftSide = false;
 
     // TODO: decide left/right margins more intelligently
-    $: style = `top: ${topPosition}px; ${isLeftSide ? "left: 50px;" : "right: 60px;"} text-align: ${isLeftSide ? "left" : "right"}`;
+    $: style = `top: ${topPosition}px; ${isLeftSide ? "left: 50px;" : "right: 60px;"} text-align: ${isLeftSide ? "left" : "right"}; font-size: ${$viewport.isLikelyInMobileLandscape ? '40px' : '56px'}`;
 </script>
 
 <div class="container" style={style} aria-label={`In ${year}, love songs made up ${percentage}% of all songs.`}>
-    <div class="percentage" style:font-size={$viewport.isLikelyInMobileLandscape ? '40px' : '56px'}>{percentage}%</div>
+    {percentage}%
 </div>   
 
 <style>
@@ -21,10 +21,6 @@
 
         z-index: 100000;
         font-family: 'Atlas Grotesk', sans-serif;
-    }
-
-    .percentage {
         font-weight: bold;
-        font-size: 56px;
     }
 </style>
