89116f4a23b91bfd74614f4ec0519b0e23c20da5
David Mora <davidnmora@gmail.com>
2024-09-15 18:35:03 -0700

    Skip non love songs for now

--- src/components/viz/LoveSongTypeLabels.svelte
+++ src/components/viz/LoveSongTypeLabels.svelte
@@ -3,34 +3,37 @@
 	import { getXPosForYear } from "$data/data-utils";
 	import { getYPosForPercentage } from "$stores/forcePositionOptions-helper";
 	import LoveSongTypeCategoryButtons from "./LoveSongTypeCategoryButtons.svelte";
-	import { LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP } from "$data/data-constants";
-	import { currentStoryStep } from "$stores/storySteps";
+	import { LOVE_SONG_TYPE_CONSTANTS, LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP } from "$data/data-constants";
+	import { aSingleLoveSongTypeIsSpotlighted, currentStoryStep } from "$stores/storySteps";
 	
     export let tweenedCoords;
 
-	$: getX = (x, width) => {
+	const getX = (x, width) => {
 		return getXPosForYear(x, width);
 	}
-	$: getY = (y0, y1, height) => {
-		return getYPosForPercentage(y0, $viewport.height) - 0.035 * $viewport.height;
+	const getY = (y0, y1, loveSongType, height) => {
+		return getYPosForPercentage(y0, height) - 0.035 * height;
 	}
 
-	$: labelMetadata = tweenedCoords.reduce((acc, { loveSongType, svgCoords }) => {
+	$: labelMetadata = tweenedCoords
+		.filter(({loveSongType}) => loveSongType !== LOVE_SONG_TYPE_CONSTANTS.notALoveSong)
+		.reduce((acc, { loveSongType, svgCoords }) => {
 			const MIN_Y_HEIGHT_TO_FIT_LABEL = 0.04;
 			// TEMP: we don't want to place labels on the very left (first) decade, since that's where the 60s annotations goes
 			const {x, y0, y1} = svgCoords.find(({y0, y1}) => (y0 - y1) >= MIN_Y_HEIGHT_TO_FIT_LABEL) || {};
+			if (!x) return acc;
 
 			return [... acc, {
 				loveSongType,
 				x: getX(x, $viewport.width),
-				y: getY(y0, y1, $viewport.height),
+				y: getY(y0, y1, loveSongType, $viewport.height),
 				opacity: $currentStoryStep.searchAndFilterState.visibleButNotSelectedLoveSongTypes.includes(loveSongType) ? 0 : 1,
 				// TODO: fixed by Michelle! use normal css-case even in svelte properties on components.
 				fontSize: (y0 - y1) < 2 * MIN_Y_HEIGHT_TO_FIT_LABEL ? "clamp(0.5rem, 2vw, .75rem)" : "clamp(1rem, 2vw, 1.25rem)"
 			}]
 		}, []);
 
-	$: show = $currentStoryStep.showXAxis && !$currentStoryStep.isFinalComparisonStep;
+	$: show = $currentStoryStep.showXAxis && !$currentStoryStep.isFinalComparisonStep && !$aSingleLoveSongTypeIsSpotlighted;
 </script>
 
 {#if show}
