53ad5943ea45159892ab3d45481dcde9a44f20b0
David Mora <davidnmora@gmail.com>
2024-09-30 16:37:17 -0700

    Select song name (matches multiple, tho)

--- src/components/search/SongSearchBar.svelte
+++ src/components/search/SongSearchBar.svelte
@@ -1,11 +1,11 @@
 <script>
-	import { SONG_DATA_COLUMNS_ENUM } from "$data/data-constants";
-	import { formatYearForDisplay } from "$data/data-utils";
-	import { selectedSongsData } from "$stores/dataDerivations";
-    import { songSearchString } from "$stores/searchAndFilter";
+    import { SONG_DATA_COLUMNS_ENUM } from "$data/data-constants";
+    import { formatYearForDisplay } from "$data/data-utils";
+    import { selectedSongsData } from "$stores/dataDerivations";
+    import { selectedSongs, songSearchString } from "$stores/searchAndFilter";
 
     import SearchBar from "./SearchBar.svelte";
-	import SongSearchResult from "./SongSearchResult.svelte";
+    import SongSearchResult from "./SongSearchResult.svelte";
 
     $: searchResults = $selectedSongsData.map(({song}) => ({
         songName: song[SONG_DATA_COLUMNS_ENUM.song],
@@ -13,11 +13,25 @@
         loveSongType: song[SONG_DATA_COLUMNS_ENUM.love_song_sub_type],
         total_weeks_in_top_10: song[SONG_DATA_COLUMNS_ENUM.total_weeks_in_top_10]
     })).sort((a, b) => b.total_weeks_in_top_10 - a.total_weeks_in_top_10);
+
+    $: handleSelectedSong = ({ songName }) => {
+        $selectedSongs = [songName];
+        $songSearchString = "";
+    }
+
+    $: handleInputFocused = () => {
+        $selectedSongs = [];
+        $songSearchString = "";
+    }
+
+    $: placeholder = $selectedSongs[0] || "Search songs...";
 </script>
 
 <SearchBar
-    placeholder="Search songs..."
+    {placeholder}
     bind:searchString={$songSearchString}
     {searchResults}
+    onResultSelected={handleSelectedSong}
+    onInputFocused={handleInputFocused}
     renderComponent={SongSearchResult}
 />
