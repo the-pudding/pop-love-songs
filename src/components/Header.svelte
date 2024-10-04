3852ae6c3296794d6162b704033cd33992fa690e
David Mora <davidnmora@gmail.com>
2024-10-03 17:03:03 -0700

    masthead is clickable now

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -17,6 +17,9 @@
 		margin: 0 auto;
 		padding: 32px 0;
 		transform: rotate(-4deg);
+		/* Apparently its getting position static, which is preventing z-index from working. Not sure why... */
+		position: relative;
+		z-index: 10000000;
 	}
 
 	.wordmark a {
@@ -27,5 +30,6 @@
 
 	.wordmark a:hover {
 		background-color: transparent;
+		opacity: 0.9;
 	}
 </style>
