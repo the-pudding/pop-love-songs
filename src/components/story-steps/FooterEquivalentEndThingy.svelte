3134566d14010c77d80fe318f8c126137dd16b05
David Mora <davidnmora@gmail.com>
2024-10-05 18:42:47 -0700

    fill in footer

--- src/components/story-steps/FooterEquivalentEndThingy.svelte
+++ src/components/story-steps/FooterEquivalentEndThingy.svelte
@@ -1,6 +1,28 @@
 <script>
+    import copy from "$data/copy";
 	import Footer from "$components/Footer.svelte";
+	import DataMethodsModal from "./DataMethodsModal.svelte";
 
+    let showModal = false;
 </script>
 
-<Footer />
+<div div class="container">
+    <div>
+        <h2>Thanks for reading!</h2>
+        {@html copy.byline}
+
+        <button on:click={() => showModal = true}> Read the methods here</button>
+        <DataMethodsModal bind:showModal />
+    </div>
+
+    <Footer />
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100vh; /* @michelle: what I want is for hte <Footer/> to stay pushed to the bottom of the page */
+    }
+</style>
