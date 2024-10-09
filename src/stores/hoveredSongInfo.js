1ecb84a0fab0897659c2c4184bcc4401a6800711
David Mora <davidnmora@gmail.com>
2024-10-09 12:33:45 -0700

    Be explicit

--- src/stores/hoveredSongInfo.js
+++ src/stores/hoveredSongInfo.js
@@ -1,8 +1,11 @@
 import { writable } from "svelte/store";
 
 const DEFAULT_STATE = {
-    song: undefined,
-    x: undefined,
-    y: undefined,
+	song: undefined,
+	x: undefined,
+	y: undefined,
+	circleX: undefined,
+	circleY: undefined,
+	circleRadius: undefined
 };
 export default writable(DEFAULT_STATE);
