e6b643b2ee0fbfda6e86617defab89ad67b90794
David Mora <davidnmora@gmail.com>
2024-10-29 17:18:07 -0700

    juust right with some good ol' magic numbers

--- src/components/viz/TotalLoveSongPercentageAnnotation.svelte
+++ src/components/viz/TotalLoveSongPercentageAnnotation.svelte
@@ -8,8 +8,8 @@
     export let percentage = 0;
     export let isLeftSide = false;
 
-    // TODO: decide left/right margins more intelligently
-    $: style = `top: ${topPosition}px; ${isLeftSide ? `left: ${$margins.left}px;` : `right: ${$margins.right}px;`} text-align: ${isLeftSide ? "left" : "right"}; font-size: ${$viewport.isLikelyInMobileLandscape ? '40px' : '56px'}`;
+    // I have no idea why the .012 is necessary, but it is :)
+    $: style = `top: ${topPosition}px; ${isLeftSide ? `left: ${$margins.left}px;` : `right: ${$margins.right + $viewport.width * 0.012}px;`} text-align: ${isLeftSide ? "left" : "right"}; font-size: ${$viewport.isLikelyInMobileLandscape ? '40px' : '56px'}`;
 </script>
 
 <li class="container" style={style} aria-label={loveSongDecadeChangeAriaLabel(decade, percentage)}>
