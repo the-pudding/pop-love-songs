5d0a1adb6c2c9b7a47dc871f540b63e17e65b703
David Mora <davidnmora@gmail.com>
2024-11-02 21:38:54 -0700

    Add headline individual images, metadata

--- src/components/story-steps/images.js
+++ src/components/story-steps/images.js
@@ -2,3 +2,27 @@ export const TITLE_CARD_BACKGROUND_IMAGE = {
 	src: "assets/images/elvis and olivia rodrigo.png",
 	alt: "Elvis Presley, smiling, and Olivia Rodrigo, scowling, side by side."
 };
+
+
+export const HEADLINES = [
+	{
+		src: "assets/images/headlines/a generation without love songs.png",
+		alt: "A Huffpost headline that reads 'A generation without love songs.'",
+		url: "https://www.huffpost.com/entry/where-is-the-love-a-generation-without-love-songs_b_5894061de4b0985224db53fa"
+	},
+	{
+		src: "assets/images/headlines/love songs are harder to find.png",
+		alt: "An Andscape headline that reads 'Where did the love go? In today's black pop music, love songs are harder to find'",
+		url: "https://andscape.com/features/where-did-the-love-go-in-todays-black-pop-music-love-songs-are-harder-to-find/"
+	},
+	{
+		src: "assets/images/headlines/love songs are in decline.png",
+		alt: "A headline from The Guardian that reads 'Love Songs are In Decline -- but at least Pharell's happy'",
+		url: "https://www.theguardian.com/commentisfree/2014/mar/21/song-lyrics-analysis-love"
+	},
+	{
+		src: "assets/images/headlines/Love Songs, RIP.png",
+		alt: "A Commentary.org headline that reads 'Love Songs, RIP'",
+		url: "https://www.commentary.org/articles/terry-teachout/love-songs-rip/"
+	}
+];
