3e690129e09795edfca7bfd429d769513a05d762
David Mora <davidnmora@gmail.com>
2024-11-04 21:41:57 -0800

    whoops

--- src/data/songs-data.js
+++ src/data/songs-data.js
@@ -1,5 +1,4 @@
 import rawSongsData from "$data/pop-love-songs-data-through-september-2023.json";
-import { SONG_DATA_COLUMNS_ENUM } from "./data-constants";
 
 // NOTE: update these if you ever expand the data range
 export const MIN_DATE = 1958.59;
@@ -8,10 +7,10 @@ export const MIN_DATE = 1958.59;
 // 	Infinity
 // );
 export const MAX_DATE = 2023.75;
-	// 	rawSongsData.reduce(
-	// 	(acc, song) => Math.max(acc, +song[SONG_DATA_COLUMNS_ENUM.date_as_decimal]),
-	// 	-Infinity
-	// );
+// 	rawSongsData.reduce(
+// 	(acc, song) => Math.max(acc, +song[SONG_DATA_COLUMNS_ENUM.date_as_decimal]),
+// 	-Infinity
+// );
 
 // Wrap each array in an object (to which force simulation will attach properties)
 const wrappedData = rawSongsData.map((song, songIndex) => ({
