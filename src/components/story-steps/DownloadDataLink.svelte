d243578b20b53c50ce7a98dfa1eb768b8ae30bb7
David Mora <davidnmora@gmail.com>
2024-11-12 18:33:33 -0800

    Fix data download, which before cutoff the data after reaching the href string maximum length

--- src/components/story-steps/DownloadDataLink.svelte
+++ src/components/story-steps/DownloadDataLink.svelte
@@ -2,7 +2,6 @@
     import copy from "$data/copy.json";
 	import { SONG_DATA_COLUMNS_ENUM, LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP, LOVE_SONG_TYPE_CONSTANTS } from "$data/data-constants";
     import songsData from "$data/songs-data.js";
-	import csvDownload from "$utils/csvDownload";
 
     const loveSongType = song => {
         const type = song[SONG_DATA_COLUMNS_ENUM.love_song_sub_type];
@@ -12,16 +11,38 @@
         return LOVE_SONG_TYPE_TO_DISPLAY_TEXT_MAP[song[SONG_DATA_COLUMNS_ENUM.love_song_sub_type]];
     }
 
-    const formattedData = songsData.map(({song}) => ({
-        performers_list_str: song[SONG_DATA_COLUMNS_ENUM.performers_list_str],
-        song: song[SONG_DATA_COLUMNS_ENUM.song],
-        date_as_decimal: song[SONG_DATA_COLUMNS_ENUM.date_as_decimal],
-        loveSongType: loveSongType(song),
-        total_weeks_in_top_10: song[SONG_DATA_COLUMNS_ENUM.total_weeks_in_top_10],
-    }));
+    // Note: these must match the order as the actual data in stringifyDatum
+    const userFriendlyColumnNames = [
+        'song_title', // SONG_DATA_COLUMNS_ENUM.song,
+        'pipe_delimited_artist_list', // SONG_DATA_COLUMNS_ENUM.performers_list_str,
+        'love_song_category', // SONG_DATA_COLUMNS_ENUM.love_song_sub_type,
+
+        'top_10_debut_date_as_decimal', // SONG_DATA_COLUMNS_ENUM.date_as_decimal,
+        
+        'total_weeks_in_top_10', // SONG_DATA_COLUMNS_ENUM.total_weeks_in_top_10,
+    ]
+
+    const escapeCsvField = field => `"${String(field).replace(/"/g, '""')}"`;
+
+    const stringifyDatum = ({song}) => ([
+        escapeCsvField(song[SONG_DATA_COLUMNS_ENUM.song]),
+        escapeCsvField(song[SONG_DATA_COLUMNS_ENUM.performers_list_str]),
+        loveSongType(song),
+        
+        song[SONG_DATA_COLUMNS_ENUM.date_as_decimal],
+        
+        song[SONG_DATA_COLUMNS_ENUM.total_weeks_in_top_10],
+    ])
+
+    const csvDownload = data => {
+        const header = userFriendlyColumnNames.join(",");
+        const csv = [header, ...data.map(stringifyDatum)].join("\n");
+        const blob = new Blob([csv], { type: "text/csv" });
+        return URL.createObjectURL(blob);
+    }
 </script>
 
-<a aria-label="Download the full dataset from this article" href={csvDownload(formattedData)} download={copy.dataDownloadFileName}>
+<a aria-label="Download the full dataset from this article" href={csvDownload(songsData)} download={copy.dataDownloadFileName}>
     <slot />
 </a>
 
